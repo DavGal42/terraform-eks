a7c3eba182e1d36a544a526266eb2a00f853a7b1
David Galstyan <galstyand.25@gmail.com>
2025-07-25 19:23:44 +0400

    Update main.tf

--- terraform-eks/main.tf
+++ terraform-eks/main.tf
@@ -1,13 +1,13 @@
-#terraform {
-#  backend "s3" {
-#    bucket         = "YOUR_S3_BUCKET_NAME"
-#    key            = "eks/terraform.tfstate"
-#    region         = var.aws_region
-#    dynamodb_table = "terraform-locks"
-#    encrypt        = true
-#  }
-#}
-#
+terraform {
+  backend "s3" {
+    bucket         = "YOUR_S3_BUCKET_NAME"
+    key            = "eks/terraform.tfstate"
+    region         = var.aws_region
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
